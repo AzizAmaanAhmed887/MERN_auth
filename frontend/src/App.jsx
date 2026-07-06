2e80b3d02837900e831259630fc74836045c4f2d
AzizAmaanAhmed887 <amanahmed1122004@gmail.com>
2026-07-06 14:22:57 +0530

    Complete authentication and authorization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
+import './index.css'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
+import Refreshandler from './RefreshHandler'
+
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to={"/login"} />
+  }
   return (
-    <div className='App'>
+    <div className="App">
+      <Refreshandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
-        <Route path='/' element={<Navigate to='/login' />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/signup' element={<Signup />} />
+        <Route path='/' element={<Navigate to="/login" />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<PrivateRoute element={<Home />} />} />
       </Routes>
     </div>
   )
